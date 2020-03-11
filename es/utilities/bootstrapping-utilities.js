cbfb333dc94bd41b84090ad3e2a34d359b4dcd4f
aron <aron@olioapps.com>
2020-03-10 18:45:40 -0700

    push

--- es/utilities/bootstrapping-utilities.js
+++ es/utilities/bootstrapping-utilities.js
@@ -3,14 +3,13 @@ function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableTo
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
 
 function _iterableToArray(iter) { 
-  debugger
-  if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") {
+  const isEdge = /Edge\/\d./i.test(navigator.userAgent);
+  if (isEdge || Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") {
     return Array.from(iter); 
   }
 }
 
 function _arrayWithoutHoles(arr) { 
-  debugger
   if (Array.isArray(arr)) { 
     for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { 
       arr2[i] = arr[i]; 
